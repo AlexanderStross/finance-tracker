8e03362237a8a92f0714f3392ee447d6ab4824af
“abstross” <abstross@pm.me>
2021-08-03 16:44:59 -0500

    stocks load

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,6 +2,7 @@ class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol)
     client = TwelvedataRuby.client(apikey: Rails.application.credentials.twelvedata_client[:api_key],
                                    connect_timeout: 300)
-    TwelvedataRuby.client.price(symbol: ticker_symbol).parsed_body[:price].to_d
+    stock = TwelvedataRuby.client.quote(symbol: ticker_symbol).parsed_body
+    new(ticker: ticker_symbol, name: stock[:name], last_price: price = stock[:close].to_d) unless stock[:code].present?
   end
 end
