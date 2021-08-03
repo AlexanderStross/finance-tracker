0c6a7d906b4f9722d67f623578c2045fe27ff479
“abstross” <abstross@pm.me>
2021-08-03 12:45:33 -0500

    Keys protected

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,7 @@
 class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol)
-    client = TwelvedataRuby.client(apikey: '64a701b0e89d4625ba108288748893f8', connect_timeout: 300)
+    client = TwelvedataRuby.client(apikey: 'Rails.application.credentials.twelvedata_client[:api_key]',
+                                   connect_timeout: 300)
     TwelvedataRuby.client.price(symbol: ticker_symbol).parsed_body[:price].to_d
   end
 end
