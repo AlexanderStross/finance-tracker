4c339b33429f92ac25594920f31096f1ce492b0d
“abstross” <abstross@pm.me>
2021-08-06 14:00:02 -0500

    added user profile, fixed sign up. I should be using branches sorry.

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,8 +1,17 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
+
   def self.new_lookup(ticker_symbol)
     client = TwelvedataRuby.client(apikey: Rails.application.credentials.twelvedata_client[:api_key],
                                    connect_timeout: 300)
     stock = TwelvedataRuby.client.quote(symbol: ticker_symbol).parsed_body
     new(ticker: ticker_symbol, name: stock[:name], last_price: price = stock[:close].to_d) unless stock[:code].present?
+  end
+
+  def self.check_db(ticker_symbol)
+    Stock.where(ticker: ticker_symbol).first
   end
 end
