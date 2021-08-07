5d1475ecf8f71b4fc8aafad5306255cc808d0419
“abstross” <abstross@pm.me>
2021-08-07 17:55:52 -0500

    fixex

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -7,10 +7,10 @@ class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol)
     client = TwelvedataRuby.client(apikey: Rails.application.credentials.twelvedata_client[:api_key],
                                    connect_timeout: 300)
-    pulled_stock = TwelvedataRuby.client.quote(symbol: ticker_symbol).parsed_body
-    unless pulled_asset[:code].present?
-      new(ticker: ticker_symbol, name: pulled_asset[:name],
-          last_price: pulled_asset[:close].to_d, exchange: pulled_asset[:exchange])
+    remote_stock = TwelvedataRuby.client.quote(symbol: ticker_symbol).parsed_body
+    unless remote_stock[:code].present?
+      new(ticker: ticker_symbol, name: remote_stock[:name],
+          last_price: remote_stock[:close].to_d, exchange: remote_stock[:exchange])
     end
   end
 
@@ -24,19 +24,19 @@ class Stock < ApplicationRecord
 
       client = TwelvedataRuby.client(apikey: Rails.application.credentials.twelvedata_client[:api_key],
                                      connect_timeout: 300)
-      pulled_stock = TwelvedataRuby.client.quote(symbol: s.ticker).parsed_body
-      s.last_price = pulled_stock[:close].to_d
-      s.exchange = pulled_stock[:exchange]
+      remote_stock = TwelvedataRuby.client.quote(symbol: s.ticker).parsed_body
+      s.last_price = remote_stock[:close].to_d
+      s.exchange = remote_stock[:echange]
       s.save
-      sleep(8) unless s == @stocks.last
+      sleep(15)
     end
 
     def self.update_price(_ticker_symbol)
       if s = where(ticker: _ticker_symbol).first && !(s.updated_at < 10.minutes.ago)
         client = TwelvedataRuby.client(apikey: Rails.application.credentials.twelvedata_client[:api_key],
                                        connect_timeout: 300)
-        pulled_stock = TwelvedataRuby.client.quote(symbol: _ticker_symbol).parsed_body
-        s.last_price = pulled_stock[:close].to_d
+        remote_stock = TwelvedataRuby.client.quote(symbol: _ticker_symbol).parsed_body
+        s.last_price = remote_stock[:close].to_d
         s.save
       end
     end
