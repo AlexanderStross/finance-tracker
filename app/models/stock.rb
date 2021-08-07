35d30e50926fde3d68e3762d8b9cd00c4ebc1d31
“abstross” <abstross@pm.me>
2021-08-07 02:42:34 -0500

    working

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -7,22 +7,37 @@ class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol)
     client = TwelvedataRuby.client(apikey: Rails.application.credentials.twelvedata_client[:api_key],
                                    connect_timeout: 300)
-    stock = TwelvedataRuby.client.quote(symbol: ticker_symbol).parsed_body
-    new(ticker: ticker_symbol, name: stock[:name], last_price: price = stock[:close].to_d) unless stock[:code].present?
+    pulled_stock = TwelvedataRuby.client.quote(symbol: ticker_symbol).parsed_body
+    unless pulled_stock[:code].present?
+      new(ticker: ticker_symbol, name: pulled_stock[:name],
+          last_price: pulled_stock[:close].to_d)
+    end
   end
 
   def self.check_db(ticker_symbol)
-    Stock.where(ticker: ticker_symbol).first
+    where(ticker: ticker_symbol).first
   end
 
   def self.update_prices
     all.each do |s|
+      next unless s.updated_at < 10.minutes.ago
+
       client = TwelvedataRuby.client(apikey: Rails.application.credentials.twelvedata_client[:api_key],
                                      connect_timeout: 300)
       pulled_stock = TwelvedataRuby.client.quote(symbol: s.ticker).parsed_body
       s.last_price = pulled_stock[:close].to_d
       s.save
-      sleep(8)
+      sleep(15)
+    end
+
+    def self.update_price(_ticker_symbol)
+      if s = where(ticker: _ticker_symbol).first && !(s.updated_at < 10.minutes.ago)
+        client = TwelvedataRuby.client(apikey: Rails.application.credentials.twelvedata_client[:api_key],
+                                       connect_timeout: 300)
+        pulled_stock = TwelvedataRuby.client.quote(symbol: _ticker_symbol).parsed_body
+        s.last_price = pulled_stock[:close].to_d
+        s.save
+      end
     end
   end
 end
